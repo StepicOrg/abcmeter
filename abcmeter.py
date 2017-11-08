131fe65c97cad920acb89f768c081d146b6bed83
meanmail <meanmail@mail.ru>
2017-11-08 19:47:00 +0700

    Refactoring

--- abcmeter.py
+++ abcmeter.py
@@ -1,30 +1,11 @@
-import os
 from contextlib import contextmanager
-from datetime import datetime
 
 from antlr4 import FileStream, CommonTokenStream, ParseTreeWalker
+
 
 CPP = 'c++'
 JAVA = 'java8'
 PYTHON = 'python3'
-
-language_by_extension = {
-    '.cpp': CPP,
-    '.java': JAVA,
-    '.py': PYTHON,
-}
-
-
-def print_with_time_label(start_time, content, end='\n'):
-    delta_time = datetime.now() - start_time
-    print(f'{format(delta_time.total_seconds(), ".5f")}: {content}', end=end)
-
-
-@contextmanager
-def print_process(start_time, process_name):
-    print_with_time_label(start_time, process_name + ' start')
-    yield
-    print_with_time_label(start_time, process_name + ' done')
 
 
 def get_lexer_parser_listener(language):
@@ -41,47 +22,28 @@ def get_lexer_parser_listener(language):
         return Java8Lexer, Java8Parser, Listener, Java8Parser.compilationUnit
 
 
-def get_language_by_file(file_name):
-    _, extension = os.path.splitext(file_name)
+@contextmanager
+def nope_printer(*args, **kwargs):
+    yield
 
-    return language_by_extension[extension]
 
-
-def calculate_abc_score(start_time, file_name, language):
+def calculate_abc_score(start_time, file_name, language, printer=nope_printer):
     Lexer, Parser, Listener, start_token = get_lexer_parser_listener(language)
 
-    with print_process(start_time, f'input from {file_name}'):
+    with printer(start_time, f'input from {file_name}'):
         input = FileStream(file_name, encoding='utf8')
 
-    with print_process(start_time, 'analysis'):
+    with printer(start_time, 'analysis'):
         lexer = Lexer(input)
         stream = CommonTokenStream(lexer)
 
-    with print_process(start_time, 'parsing'):
+    with printer(start_time, 'parsing'):
         parser = Parser(stream)
         tree = start_token(parser)
 
-    with print_process(start_time, 'calculate score'):
+    with printer(start_time, 'calculate score'):
         listener = Listener()
         walker = ParseTreeWalker()
         walker.walk(listener, tree)
 
     return listener
-
-
-def main():
-    file_name = 'cpp/tests/main.cpp'
-    language = get_language_by_file(file_name)
-
-    start_time = datetime.now()
-
-    listener = calculate_abc_score(start_time, file_name, language)
-
-    print_with_time_label(start_time, 'a: ' + str(listener.a))
-    print_with_time_label(start_time, 'b: ' + str(listener.b))
-    print_with_time_label(start_time, 'c: ' + str(listener.c))
-    print_with_time_label(start_time, 'ABC score: ' + str(listener.abc_score))
-
-
-if __name__ == '__main__':
-    main()
---
+++ tests.py
@@ -0,0 +1,48 @@
+from contextlib import contextmanager
+from datetime import datetime
+
+import os
+
+from abcmeter import CPP, JAVA, PYTHON, calculate_abc_score
+
+
+def print_with_time_label(start_time, content, end='\n'):
+    delta_time = datetime.now() - start_time
+    print(f'{format(delta_time.total_seconds(), ".5f")}: {content}', end=end)
+
+
+@contextmanager
+def print_process(start_time, process_name):
+    print_with_time_label(start_time, process_name + ' start')
+    yield
+    print_with_time_label(start_time, process_name + ' done')
+
+
+language_by_extension = {
+    '.cpp': CPP,
+    '.java': JAVA,
+    '.py': PYTHON,
+}
+
+
+def get_language_by_file(file_name):
+    _, extension = os.path.splitext(file_name)
+
+    return language_by_extension[extension]
+
+def main():
+    file_name = 'cpp/tests/main.cpp'
+    language = get_language_by_file(file_name)
+
+    start_time = datetime.now()
+
+    listener = calculate_abc_score(start_time, file_name, language, print_process)
+
+    print_with_time_label(start_time, 'a: ' + str(listener.a))
+    print_with_time_label(start_time, 'b: ' + str(listener.b))
+    print_with_time_label(start_time, 'c: ' + str(listener.c))
+    print_with_time_label(start_time, 'ABC score: ' + str(listener.abc_score))
+
+
+if __name__ == '__main__':
+    main()
